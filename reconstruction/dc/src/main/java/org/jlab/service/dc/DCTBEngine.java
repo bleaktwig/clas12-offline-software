0b5071d4e9d2f1bdfba5cdc058c25be59f60a68a
bleaktwig <bleak.twig@gmail.com>
2018-09-27 13:53:14 -0300

    changed the way DCKF works and optimized the RungeKutta class

--- reconstruction/dc/src/main/java/org/jlab/service/dc/DCTBEngine.java
+++ reconstruction/dc/src/main/java/org/jlab/service/dc/DCTBEngine.java
@@ -43,8 +43,6 @@ public class DCTBEngine extends DCEngine {
     public DCTBEngine() {
         super("DCTB");
         tde = new TimeToDistanceEstimator();
-        KFen = new DCKFEngine();
-        KFen.init();
     }
 
     @Override
@@ -55,8 +53,7 @@ public class DCTBEngine extends DCEngine {
 
     @Override
     public boolean processDataEvent(DataEvent event) {
-        // setRunConditionsParameters(event);
-        if(event.hasBank("RUN::config") == false) {
+        if (event.hasBank("RUN::config") == false) {
             System.err.println("RUN CONDITIONS NOT READ AT TIMEBASED LEVEL!");
             return true;
         }
@@ -66,23 +63,24 @@ public class DCTBEngine extends DCEngine {
         //    return true; // require the start time to reconstruct the tracks in the event
 
         DataBank bank = event.getBank("RUN::config");
+
         // Load the constants
-        // ------------------
         int newRun = bank.getInt("run", 0);
-        if(newRun == 0) return true;
+        if (newRun == 0) return true;
 
         double T_Start = 0;
-        if(Constants.isUSETSTART() == true) {
-            if(event.hasBank("RECHB::Event") == true) {
+        if (Constants.isUSETSTART() == true) {
+            if (event.hasBank("RECHB::Event") == true) {
                 T_Start = event.getBank("RECHB::Event").getFloat("STTime", 0);
-                if(T_Start<0) return true; // quit if start time not found in data
+                // Quit if start time not found in data
+                if (T_Start < 0) return true;
             } else {
-                return true; // no REC HB bank
+                return true; // No REC HB bank
             }
         }
-        // get Field
-        Swim dcSwim = new Swim();
-        // System.out.println(" RUNNING TIME BASED....................................");
+
+        // Get field
+        Swim dcSwim                = new Swim();
         ClusterFitter cf           = new ClusterFitter();
         ClusterCleanerUtilities ct = new ClusterCleanerUtilities();
 
@@ -92,85 +90,96 @@ public class DCTBEngine extends DCEngine {
         List<Cross> crosses          = new ArrayList<Cross>();
         List<Track> trkcands         = new ArrayList<Track>();
 
-        //instantiate bank writer
+        // Instantiate bank writer
         RecoBankWriter rbc = new RecoBankWriter();
 
-        HitReader hitRead = new HitReader();
+        HitReader hitRead  = new HitReader();
         hitRead.read_HBHits(event,
-            super.getConstantsManager().getConstants(newRun,
-                "/calibration/dc/signal_generation/doca_resolution"),
-            super.getConstantsManager().getConstants(newRun,
-                "/calibration/dc/time_to_distance/time2dist"),
-            Constants.getT0(), Constants.getT0Err(), dcDetector, tde);
+            super.getConstantsManager()
+                 .getConstants(newRun,
+                               "/calibration/dc/signal_generation/doca_resolution"),
+            super.getConstantsManager()
+                 .getConstants(newRun,
+                               "/calibration/dc/time_to_distance/time2dist"),
+            Constants.getT0(), Constants.getT0Err(), dcDetector, tde
+        );
+
         hitRead.read_TBHits(event,
-            super.getConstantsManager().getConstants(newRun,
-                "/calibration/dc/signal_generation/doca_resolution"),
-            super.getConstantsManager().getConstants(newRun,
-                "/calibration/dc/time_to_distance/time2dist"),
-            tde, Constants.getT0(), Constants.getT0Err());
+            super.getConstantsManager()
+                 .getConstants(newRun,
+                               "/calibration/dc/signal_generation/doca_resolution"),
+            super.getConstantsManager()
+                 .getConstants(newRun,
+                               "/calibration/dc/time_to_distance/time2dist"),
+                               tde, Constants.getT0(), Constants.getT0Err()
+        );
+
         List<FittedHit> hits = new ArrayList<FittedHit>();
-        // I) get the hits
-        if(hitRead.get_TBHits().isEmpty()) hits = hitRead.get_HBHits();
-        else                               hits = hitRead.get_TBHits();
+// I) Get the hits =============================================================
+        // if (hitRead.get_TBHits().isEmpty())
+        //     hits = hitRead.get_HBHits();
+        // else
+        //     hits = hitRead.get_TBHits();
+        hits = hitRead.get_HBHits();
 
-        // II) process the hits
-        // 1) exit if hit list is empty
-        if(hits.isEmpty()) return true;
+// II) Process the hits ========================================================
+// 1)  Exit if hit list is empty -----------------------------------------------
+        if(hits.isEmpty() ) {
+                return true;
+        }
 
-        // 2) find the clusters from these hits
+// 2)  Find the clusters from these hits ---------------------------------------
         ClusterFinder clusFinder = new ClusterFinder();
 
         clusters = clusFinder.FindTimeBasedClusters(hits, cf, ct,
-            super.getConstantsManager().getConstants(newRun,
-                "/calibration/dc/time_to_distance/time2dist"), dcDetector, tde);
+            super.getConstantsManager()
+                 .getConstants(newRun,
+                               "/calibration/dc/time_to_distance/time2dist"),
+                               dcDetector, tde);
 
-        if(clusters.isEmpty()) {
+        if (clusters.isEmpty()) {
             rbc.fillAllTBBanks(event, rbc, hits, null, null, null, null);
             return true;
         }
 
-        // 3) find the segments from the fitted clusters
+// 3)  Find the segments from the fitted clusters ------------------------------
         SegmentFinder segFinder = new SegmentFinder();
 
         List<FittedCluster> pclusters = segFinder.selectTimeBasedSegments(clusters);
 
-        segments =  segFinder.get_Segments(pclusters, event, dcDetector, false);
+        segments = segFinder.get_Segments(pclusters, event, dcDetector, false);
 
-        if(segments.isEmpty()) { // need 6 segments to make a trajectory
-            for(FittedCluster c : clusters) {
-                for(FittedHit hit : c) {
+        if (segments.isEmpty()) { // 6 segments are needed to make a trajectory
+            for (FittedCluster c : clusters) {
+                for (FittedHit hit : c) {
                     hit.set_AssociatedClusterID(c.get_Id());
                     hit.set_AssociatedHBTrackID(c.get(0).get_AssociatedHBTrackID());
                     fhits.add(hit);
                 }
             }
-            rbc.fillAllTBBanks( event, rbc, fhits, clusters, null, null, null);
+            rbc.fillAllTBBanks(event, rbc, fhits, clusters, null, null, null);
             return true;
         }
 
-        for(Segment seg : segments) {
-            for(FittedHit hit : seg.get_fittedCluster()) {
+        for (Segment seg : segments) {
+            for (FittedHit hit : seg.get_fittedCluster()) {
                 fhits.add(hit);
             }
         }
 
         CrossMaker crossMake = new CrossMaker();
-        // crosses = crossMake.find_Crosses(segments, dcDetector);
 
-        // if(crosses.isEmpty() ) {
-        //     rbc.fillAllTBBanks(event, rbc, fhits, clusters, segments, null, null);
-        //     return true;
-        // }
-
-        // also need Track bank
-        if (event.hasBank("HitBasedTrkg::HBTracks") == false) return true;
+        // Also need Track bank
+        if (event.hasBank("HitBasedTrkg::HBTracks") == false) {
+            return true;
+        }
 
         DataBank trkbank    = event.getBank("HitBasedTrkg::HBTracks");
         DataBank trkcovbank = event.getBank("TimeBasedTrkg::TBCovMat");
         int trkrows = trkbank.rows();
-        if(trkbank.rows() != trkcovbank.rows()) {
-            return true; // HB tracks not saved correctly
-        }
+
+        if (trkbank.rows() != trkcovbank.rows()) return true; // HB tracks not saved correctly
+
         Track[] TrackArray = new Track[trkrows];
         for (int i = 0; i < trkrows; i++) {
             Track HBtrk = new Track();
@@ -180,9 +189,11 @@ public class DCTBEngine extends DCEngine {
             HBtrk.set_pAtOrig(new Vector3D(trkbank.getFloat("p0_x", i),
                                            trkbank.getFloat("p0_y", i),
                                            trkbank.getFloat("p0_z", i)));
+
             HBtrk.set_Vtx0(new Point3D(trkbank.getFloat("Vtx0_x", i),
                                        trkbank.getFloat("Vtx0_y", i),
                                        trkbank.getFloat("Vtx0_z", i)));
+
             HBtrk.set_FitChi2(trkbank.getFloat("chi2", i));
             Matrix initCMatrix = new Matrix(new double[][]{
                 {trkcovbank.getFloat("C11", i), trkcovbank.getFloat("C12", i),
@@ -201,38 +212,80 @@ public class DCTBEngine extends DCEngine {
                  trkcovbank.getFloat("C53", i), trkcovbank.getFloat("C54", i),
                  trkcovbank.getFloat("C55", i)}
             });
+
             HBtrk.set_CovMat(initCMatrix);
             TrackArray[HBtrk.get_Id()-1] = HBtrk;
             TrackArray[HBtrk.get_Id()-1].set_Status(0);
         }
-        if(TrackArray == null) return true; // HB tracks not saved correctly
+        if (TrackArray == null) return true; // HB tracks not saved correctly
 
-        for(Segment seg : segments) {
+        for (Segment seg : segments) {
             TrackArray[seg.get(0).get_AssociatedHBTrackID()-1].get_ListOfHBSegments().add(seg);
-            if(seg.get_Status() == 1)
+            if (seg.get_Status() == 1)
                 TrackArray[seg.get(0).get_AssociatedHBTrackID()-1].set_Status(1);
         }
 
-        // 6) find the list of track candidates
+// 4)  Find the list of track candidates ---------------------------------------
         TrackCandListFinder trkcandFinder = new TrackCandListFinder("TimeBased");
+        TrajectoryFinder trjFind = new TrajectoryFinder();
+        for (int i = 0; i < TrackArray.length; i++) {
+            if (TrackArray[i] == null ||
+                TrackArray[i].get_ListOfHBSegments() == null ||
+                TrackArray[i].get_ListOfHBSegments().size() < 4) {
+                continue;
+            }
 
-        KFen.runKalmanFilter(TrackArray, crossMake, crosses,
-                             dcSwim, trkcandFinder, trkcands);
+            TrackArray[i].set_MissingSuperlayer(get_Status(TrackArray[i]));
+            TrackArray[i].addAll(crossMake.find_Crosses(TrackArray[i].get_ListOfHBSegments(),
+                                                        dcDetector));
+            if (TrackArray[i].size() < 1) continue;
+            crosses.addAll(TrackArray[i]);
 
-        for(int i = 0; i < crosses.size(); i++) {
-            crosses.get(i).set_Id(i+1);
+            KFitter kFit = new KFitter(TrackArray[i], dcDetector, true, dcSwim);
+
+            StateVec fn = new StateVec();
+            kFit.runFitter(TrackArray[i].get(0).get_Sector());
+
+            if (kFit.setFitFailed == false && kFit.finalStateVec != null) {
+                // Set the state vector at the last measurement site
+                fn.set(kFit.finalStateVec.x,  kFit.finalStateVec.y,
+                       kFit.finalStateVec.tx, kFit.finalStateVec.ty);
+
+                // Set the track parameters if the filter does not fail
+                TrackArray[i].set_P(1./Math.abs(kFit.finalStateVec.Q));
+                TrackArray[i].set_Q((int)Math.signum(kFit.finalStateVec.Q));
+                trkcandFinder.setTrackPars(TrackArray[i], new Trajectory(),
+                                           trjFind, fn,
+                                           kFit.finalStateVec.z,
+                                           dcDetector, dcSwim);
+
+                // Candidate parameters are set from the state vector
+                TrackArray[i].set_FitChi2(kFit.chi2);
+                TrackArray[i].set_FitNDF(kFit.NDF);
+                TrackArray[i].set_Trajectory(kFit.kfStateVecsAlongTrajectory);
+                TrackArray[i].set_FitConvergenceStatus(kFit.ConvStatus);
+                TrackArray[i].set_Id(TrackArray[i].size()+1);
+                TrackArray[i].set_CovMat(kFit.finalCovMat.covMat);
+                if (TrackArray[i].get_Vtx0().toVector3D().mag() > 500) continue;
+                trkcands.add(TrackArray[i]);
+            }
         }
 
-        // track found
+        for (int i = 0; i < crosses.size(); i++) {
+            crosses.get(i).set_Id(i + 1);
+        }
+
+        // Track found
         int trkId = 1;
 
-        if(trkcands.size() > 0) {
-            // trkcandFinder.removeOverlappingTracks(trkcands);		// remove overlaps
+        if (trkcands.size() > 0) {
+            // trkcandFinder.removeOverlappingTracks(trkcands); // remove overlaps
 
-            for(Track trk: trkcands) {
-                // reset the id
+            for (Track trk : trkcands) {
+                // Reset the id
                 trk.set_Id(trkId);
-                trkcandFinder.matchHits(trk.get_Trajectory(), trk, dcDetector, dcSwim);
+                trkcandFinder.matchHits(trk.get_Trajectory(),
+                                        trk, dcDetector, dcSwim);
                 trk.calcTrajectory(trkId, dcSwim,
                                    trk.get_Vtx0().x(),    trk.get_Vtx0().y(),
                                    trk.get_Vtx0().z(),    trk.get_pAtOrig().x(),
@@ -258,7 +311,7 @@ public class DCTBEngine extends DCEngine {
                     for(FittedHit h1 : c.get_Segment1()) {
                         h1.set_AssociatedTBTrackID(trk.get_Id());
                     }
-                    for(FittedHit h2 : c.get_Segment2()) {
+                    for (FittedHit h2 : c.get_Segment2()) {
                         h2.set_AssociatedTBTrackID(trk.get_Id());
                     }
                 }
@@ -266,8 +319,9 @@ public class DCTBEngine extends DCEngine {
             }
         }
 
-        if(trkcands.isEmpty()) {
-            // no cand found, stop here and save the hits, the clusters, the segments, the crosses
+        if (trkcands.isEmpty()) {
+            // No candidates found, stop here and save the hits, the clusters, the
+            //     segments and the crosses.
             rbc.fillAllTBBanks(event, rbc, fhits, clusters, segments, crosses, null);
         }
         else {
@@ -281,11 +335,10 @@ public class DCTBEngine extends DCEngine {
 
         int L[] = new int[6];
         for(int l = 0; l < track.get_ListOfHBSegments().size(); l++) {
-            L[track.get_ListOfHBSegments().get(l).get_Superlayer()-1]++;
+            L[track.get_ListOfHBSegments().get(l).get_Superlayer() - 1]++;
         }
         for(int l = 0; l < 6; l++) {
-            if(L[l] == 0)
-                miss = l + 1;
+            if(L[l] == 0) miss = l + 1;
         }
         return miss;
     }
