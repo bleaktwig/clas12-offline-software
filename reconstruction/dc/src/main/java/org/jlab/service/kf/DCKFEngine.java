89c88034476a2b2611c48f54a36eb5c4f247d39d
bleaktwig <bleak.twig@gmail.com>
2018-10-17 11:09:21 -0300

    Wrote a new RecoBankReader class and cleaned the RecoBankWriter class

--- reconstruction/dc/src/main/java/org/jlab/service/kf/DCKFEngine.java
+++ reconstruction/dc/src/main/java/org/jlab/service/kf/DCKFEngine.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import org.jlab.io.base.DataBank;
 import org.jlab.io.base.DataEvent;
 import org.jlab.geom.prim.Point3D;
@@ -16,6 +17,7 @@ import org.jlab.clas.swimtools.Swimmer;
 import org.jlab.clas.reco.ReconstructionEngine;
 import org.jlab.rec.dc.Constants;
 import org.jlab.rec.dc.banks.RecoBankWriter;
+import org.jlab.rec.dc.banks.RecoBankReader;
 import org.jlab.rec.dc.hit.FittedHit;
 import org.jlab.rec.dc.cluster.FittedCluster;
 import org.jlab.rec.dc.segment.Segment;
@@ -27,13 +29,13 @@ import org.jlab.rec.dc.track.TrackCandListFinder;
 
 /**
  * An engine to run the Kalman filtering process for the DC software.
- *
  * @author benkel
  */
 public class DCKFEngine extends ReconstructionEngine {
 
     DCGeant4Factory dcDetector;
-    private int dataeventCounter;
+    private int eventCounter;
+    private int debug = 2;
 
     public DCKFEngine() {
         super("DCKF", "benkel", "0.11");
@@ -53,6 +55,7 @@ public class DCKFEngine extends ReconstructionEngine {
             // "/calibration/dc/time_jitter",
             // "/calibration/dc/tracking/wire_status",
         };
+        // Load constants
         requireConstants(Arrays.asList(dcTables));
 
         // Load geometry
@@ -63,15 +66,17 @@ public class DCKFEngine extends ReconstructionEngine {
                                              Optional.ofNullable(geomDBVar).orElse("default"));
         dcDetector = new DCGeant4Factory(provider, DCGeant4Factory.MINISTAGGERON);
 
-        dataeventCounter = -1;
+        eventCounter = -1;
         return true;
     }
 
     @Override
     public boolean processDataEvent(DataEvent event) {
-        dataeventCounter++;
-        System.out.println("[DCKF] Summary for DataEvent number " +
-                           dataeventCounter + ":");
+        eventCounter++;
+        if (debug >= 1) {
+            System.out.println("[DCKF] Summary for DataEvent number " +
+                               eventCounter + ":");
+        }
 
 // === INITIAL CHECKUP =========================================================
         if (!event.hasBank("RUN::config")) return true;
@@ -81,35 +86,52 @@ public class DCKFEngine extends ReconstructionEngine {
         if (newRun == 0) return true;
 
         Swim dcSwim = new Swim();
-        RecoBankWriter rbc = new RecoBankWriter();
-        System.out.println("[DCKF] " + dataeventCounter + ".1: initialization done.");
-
+        RecoBankWriter rbw = new RecoBankWriter();
+        RecoBankReader rbr = new RecoBankReader();
+        if (debug >= 1) {
+            System.out.println("[DCKF] " + eventCounter + ".1: initialization done.");
+        }
 // === GET CROSSES =============================================================
         if (!event.hasBank("HitBasedTrkg::HBCrosses")  ||
             !event.hasBank("HitBasedTrkg::HBSegments") ||
             !event.hasBank("HitBasedTrkg::HBClusters") ||
             !event.hasBank("HitBasedTrkg::HBHits")) {
+
+            System.out.println("[DCKF] ERROR: The banks were not written " +
+                               "correctly in the DCHB1 engine.");
             return true;
         }
         DataBank crossesBank  = event.getBank("HitBasedTrkg::HBCrosses");
         DataBank segmentsBank = event.getBank("HitBasedTrkg::HBSegments");
         DataBank clustersBank = event.getBank("HitBasedTrkg::HBClusters");
         DataBank hitsBank     = event.getBank("HitBasedTrkg::HBHits");
-        System.out.println("[DCKF] " + dataeventCounter + ".2: banks loaded.");
+        if (eventCounter == 1 && debug >= 2) {
+            crossesBank.show();
+            segmentsBank.show();
+            clustersBank.show();
+            hitsBank.show();
+        }
 
-        // pull the crosses from the bank.
+        if (debug >= 1) {
+            System.out.println("[DCKF] " + eventCounter + ".2: banks loaded.");
+        }
+        // Pull the crosses from the bank.
         int nCrosses = crossesBank.rows();
-        if (nCrosses == 0) return true;
+        if (nCrosses == 0) {
+            System.out.println("[DCKF] ERROR: No crosses found in " +
+                               "HitBasedTrkg::HBCrosses bank.");
+            return true;
+        }
         List<Cross> crosses = new ArrayList();
 
         // TODO: after all is up and running I should check what data from the
         //       crosses/segments/clusters/hits I can ignore so that I minimize
         //       what RecoBankReader has to read and accelerate the whole ordeal
         for (int c = 0; c < nCrosses; c++) {
-            crosses.add(getCross(crossesBank, segmentsBank,
-                                 clustersBank, hitsBank, c));
+            crosses.add(rbr.getCross(crossesBank, segmentsBank,
+                                     clustersBank, hitsBank, c));
 
-            if (dataeventCounter == 0) {
+            if (eventCounter == 1 && debug >= 2) {
                 /**************************************************************/
                 System.out.println(
                     "      Cross " + crosses.get(c).get_Id() + " retrieved. Data:\n" +
@@ -142,9 +164,10 @@ public class DCKFEngine extends ReconstructionEngine {
                 /**************************************************************/
             }
         }
-        System.out.println("[DCKF] " + dataeventCounter + ".3: crosses loaded.");
-        // TODO: remove crosses and segments and clusters and hits from banks.
-        //       TODO: Check what is really necessary to remove from the banks.
+        if (debug >= 1) {
+            System.out.println("[DCKF] " + eventCounter + ".3: crosses loaded.");
+        }
+        // TODO: remove crosses, segments, clusters and hits from banks.
 
 // === CREATE CROSSLIST FROM CROSSES ===========================================
         CrossListFinder crossLister = new CrossListFinder();
@@ -155,253 +178,37 @@ public class DCKFEngine extends ReconstructionEngine {
                 dcDetector,
                 null,
                 dcSwim);
-        System.out.println("[DCKF] " + dataeventCounter + ".4: cross lists found.");
+        if (debug >= 1) {
+            System.out.println("[DCKF] " + eventCounter + ".4: cross lists found.");
+        }
 
-// === CREATE AND RUN TrackCandListFinder ======================================
+// === INSTANCE AND RUN TrackCandListFinder ====================================
         TrackCandListFinder trkCandFinder = new TrackCandListFinder(Constants.HITBASE);
         List<Track> trkcands = trkCandFinder.getTrackCands(crosslist,
                                                            dcDetector,
                                                            Swimmer.getTorScale(),
                                                            dcSwim);
-        System.out.println("[DCKF] " + dataeventCounter + ".5: tracks found.");
-
+        if (debug >= 1) {
+            System.out.println("[DCKF] " + eventCounter + ".5: tracks found.");
+        }
         if (trkcands.size() > 0) {
             trkCandFinder.removeOverlappingTracks(trkcands);
-            rbc.fillHBTracksBank(event, trkcands);
-            System.out.println("[DCKF] " + dataeventCounter + ".6: tracks " +
-                               "written on bank.");
+            rbw.fillHBTracksBank(event, trkcands);
+            rbw.fillTrackCovMatBank(event, trkcands);
+            if (debug >= 1) {
+                System.out.println("[DCKF] " + eventCounter + ".6: tracks " +
+                                   "written on bank.");
+            }
         }
-        else {
-            System.out.println("[DCKF] " + dataeventCounter + ".6: no tracks" +
+        else if (debug >= 1) {
+            System.out.println("[DCKF] " + eventCounter + ".6: no tracks" +
                                " found.");
         }
 
-        // TODO: Check if the covariance matrices also need to be serialized.
-
-        System.out.println("[DCKF] Processed evio event " +
-                           dataeventCounter + "\n");
+        if (debug >= 1) {
+            System.out.println("[DCKF].7 processed evio event " +
+                               eventCounter + " successfully!\n");
+        }
         return true;
-    }
-
-    // TODO: Move these get methods to RecoBankReader
-    // TODO: Write a manager method to avoid creating referenced objects
-    //       multiple times.
-    private Cross getCross(DataBank crBank, DataBank sBank,
-                           DataBank clBank, DataBank hBank,
-                           int idx) {
-        if (crBank.rows() == 0) {
-            System.out.println("[DCKF] ERROR: crosses bank is empty.");
-            return null;
-        }
-        if (idx > crBank.rows()) {
-            System.out.println("[DCKF] ERROR: index given is greater than " +
-                               "crosses bank's size.");
-            return null;
-        }
-        Cross cross = new Cross((int)  crBank.getByte ("sector", idx),
-                                (int)  crBank.getByte ("region", idx),
-                                (int)  crBank.getShort("id",     idx));
-
-        cross.set_Point   (new Point3D(crBank.getFloat("x",      idx),
-                                       crBank.getFloat("y",      idx),
-                                       crBank.getFloat("z",      idx)));
-        cross.set_PointErr(new Point3D(crBank.getFloat("err_x",  idx),
-                                       crBank.getFloat("err_y",  idx),
-                                       crBank.getFloat("err_z",  idx)));
-        cross.set_Dir     (new Point3D(crBank.getFloat("ux",     idx),
-                                       crBank.getFloat("uy",     idx),
-                                       crBank.getFloat("uz",     idx)));
-        cross.set_DirErr  (new Point3D(crBank.getFloat("err_ux", idx),
-                                       crBank.getFloat("err_uy", idx),
-                                       crBank.getFloat("err_uz", idx)));
-
-        // get the segments
-        int nSegments = sBank.rows();
-        if (nSegments == 0) {
-            System.out.println("[DCKF] ERROR: segments bank is empty.");
-            return null;
-        }
-
-        for (int i = 1; i < 3; i++) {
-            int sID = (int) crBank.getShort("Segment" + i + "_ID", idx);
-            int sAddress = -1;
-            for (int j = 0; j < nSegments; j++) {
-                if (sID == (int) sBank.getShort("id", j)) {
-                    sAddress = j;
-                    break;
-                }
-            }
-            if (sAddress == -1) {
-                System.out.println("[DCKF] ERROR: segment id was not found " +
-                                   "in bank.");
-                return null;
-            }
-
-            Segment segment = getSegment(sBank, clBank, hBank, sAddress);
-
-            if (i == 1)      cross.set_Segment1(segment);
-            else if (i == 2) cross.set_Segment2(segment);
-        }
-
-        if (cross.get_Segment1().get_Id() == -1 ||
-            cross.get_Segment2().get_Id() == -1) {
-            cross.isPseudoCross = true;
-        }
-
-        cross.set_CrossDirIntersSegWires();
-
-        return cross;
-    }
-
-    private Segment getSegment(DataBank sBank, DataBank clBank,
-                               DataBank hBank, int sAddr) {
-        if (sBank.rows() == 0) {
-            System.out.println("[DCKF] ERROR: segments bank is empty.");
-            return null;
-        }
-        if (sAddr >= sBank.rows()) {
-            System.out.println("[DCKF] ERROR: index given is greater than " +
-                               "segments bank's size.");
-            return null;
-        }
-        int nClusters = clBank.rows();
-        if (nClusters == 0) {
-            System.out.println("[DCKF] ERROR: clusters bank is empty.");
-            return null;
-        }
-        int clID = (int) sBank.getShort("Cluster_ID", sAddr);
-        int clAddress = -1;
-        for (int i = 0; i < nClusters; i++) {
-            if (clID == (int) clBank.getShort("id", i)) {
-                clAddress = i;
-                break;
-            }
-        }
-        if (clAddress == -1) {
-            System.out.println("[DCKF] ERROR: cluster id was not found in " +
-                               " clusters bank.");
-            return null;
-        }
-
-        FittedCluster fCluster = getCluster(clBank, hBank, clAddress);
-
-        Segment segment = new Segment(fCluster);
-
-        double[] endPoints = {(double) sBank.getFloat("SegEndPoint1X", sAddr),
-                              (double) sBank.getFloat("SegEndPoint1Z", sAddr),
-                              (double) sBank.getFloat("SegEndPoint2X", sAddr),
-                              (double) sBank.getFloat("SegEndPoint2Z", sAddr)};
-
-        segment.set_SegmentEndPoints(endPoints);
-
-        // TODO: compare this data with the one obtained from the cluster. Maybe
-        //       I'm rewriting the same data onto the object.
-        segment.get_fittedCluster()
-               .set_clusterLineFitSlope((double) sBank.getFloat("fitSlope", sAddr));
-        segment.get_fittedCluster()
-               .set_clusterLineFitSlopeErr((double) sBank.getFloat("fitSlopeErr", sAddr));
-        segment.get_fittedCluster()
-               .set_clusterLineFitInterceptErr((double) sBank.getFloat("fitInterc", sAddr));
-        segment.get_fittedCluster()
-               .set_clusterLineFitInterceptErr((double) sBank.getFloat("fitIntercErr", sAddr));
-
-        /* WARNING: SEGMENT DATA IN BANK CURRENTLY UNUSED:
-        bank.setFloat("avgWire", i, (float) cls.getAvgwire());
-        bank.setByte("size", i, (byte) seglist.get(i).size());
-        bank.setFloat("fitChisqProb", i, (float) ProbChi2perNDF.prob(chi2, seglist.get(i).size() - 2));
-        */
-        return segment;
-    }
-
-    private FittedCluster getCluster(DataBank clBank, DataBank hBank, int clAddr) {
-        if (clBank.rows() == 0) {
-            System.out.println("[DCKF] ERROR: clusters bank is empty.");
-            return null;
-        }
-        if (clAddr >= clBank.rows()) {
-            System.out.println("[DCKF] ERROR: index given is great than " +
-                               "clusters bank's size.");
-            return null;
-        }
-        ArrayList<Integer> hIDs = new ArrayList<Integer>();
-        ArrayList<FittedHit> fHits = new ArrayList<FittedHit>();
-        for (int i = 1; i < clBank.getByte("size", clAddr) + 1; i++) {
-            hIDs.add((int) clBank.getShort("Hit" + i + "_ID", clAddr));
-        }
-
-        int nHits = hBank.rows();
-        if (nHits == 0) {
-            System.out.println("[DCKF] ERROR: hits bank is empty.");
-            return null;
-        }
-        for (int hID : hIDs) {
-            int hAddress = -1;
-            for (int i = 0; i < nHits; i++) {
-                if (hID == (int) hBank.getShort("id", i)) {
-                    hAddress = i;
-                    break;
-                }
-            }
-            if (hAddress == -1) {
-                System.out.println("[DCKF] ERROR: hit id was not found in " +
-                                   "hits bank");
-                return null;
-            }
-            FittedHit fHit = getHit(hBank, hAddress);
-            fHits.add(fHit);
-        }
-
-        FittedCluster cluster = new FittedCluster((int) clBank.getByte ("sector",       clAddr),
-                                                  (int) clBank.getByte ("superlayer",   clAddr),
-                                                  (int) clBank.getShort("id",           clAddr),
-                                                  fHits);
-
-        cluster.set_clusterLineFitSlope       ((double) clBank.getFloat("fitSlope",     clAddr));
-        cluster.set_clusterLineFitSlopeErr    ((double) clBank.getFloat("fitSlopeErr",  clAddr));
-        cluster.set_clusterLineFitIntercept   ((double) clBank.getFloat("fitInterc",    clAddr));
-        cluster.set_clusterLineFitInterceptErr((double) clBank.getFloat("fitIntercErr", clAddr));
-
-        /* WARNING: CLUSTER DATA IN BANK CURRENTLY UNUSED:
-        bank.setShort("status", i, (short) status);
-        bank.setFloat("avgWire", i, (float) cluslist.get(i).getAvgwire());
-        bank.setFloat("fitChisqProb", i, (float) ProbChi2perNDF.prob(chi2, cluslist.get(i).size() - 2));
-        */
-
-        return cluster;
-    }
-
-    private FittedHit getHit(DataBank bank, int addr) {
-        if (bank.rows() == 0) {
-            System.out.println("[DCKF] ERROR: hits bank is empty.");
-            return null;
-        }
-        if (bank.rows() == 0 || addr >= bank.rows()) {
-            System.out.println("[DCKF] ERROR: id given is greater than hits " +
-                               "bank's size.");
-            return null;
-        }
-
-        FittedHit hit = new FittedHit((int) bank.getByte ("sector",     addr),
-                                      (int) bank.getByte ("superlayer", addr),
-                                      (int) bank.getByte ("layer",      addr),
-                                      (int) bank.getShort("wire",       addr),
-                                            bank.getInt  ("TDC",        addr),
-                                      (int) bank.getShort("id",         addr));
-
-        hit.set_TrkgStatus         ((int)    bank.getShort("status",    addr));
-        hit.set_DocaErr            ((double) bank.getFloat("docaError", addr));
-        hit.set_ClusFitDoca        ((double) bank.getFloat("trkDoca",   addr));
-        hit.set_lX                 ((double) bank.getFloat("LocX",      addr));
-        hit.set_lY                 ((double) bank.getFloat("LocY",      addr));
-        hit.set_X                  ((double) bank.getFloat("X",         addr));
-        hit.set_Z                  ((double) bank.getFloat("Z",         addr));
-        hit.set_LeftRightAmb       ((int)    bank.getByte ("LR",        addr));
-        hit.set_AssociatedClusterID((int)    bank.getShort("clusterID", addr));
-        hit.set_AssociatedHBTrackID((int)    bank.getByte ("trkID",     addr));
-        hit.setB                   ((double) bank.getFloat("B",         addr));
-        hit.setTProp               ((double) bank.getFloat("TProp",     addr));
-        hit.setTFlight             ((double) bank.getFloat("TFlight",   addr));
-
-        return hit;
     }
 }
